d941ba46915c507c1f5c3547814937f5352c2044
simon-truhter <simon-truhter@users.noreply.github.com>
2023-04-29 06:17:42 +0200

    changed the way extension being backed in 	modified:   download-extensions.sh

--- scripts/download-extensions.sh
+++ scripts/download-extensions.sh
@@ -1,9 +1,12 @@
 wget -P /tmp https://extensions.gnome.org/extension-data/appindicatorsupportrgcjonas.gmail.com.v53.shell-extension.zip 
 wget -P /tmp https://extensions.gnome.org/extension-data/custom-accent-colorsdemiskp.v6.shell-extension.zip
 wget -P /tmp https://extensions.gnome.org/extension-data/soft-brightness-plusjoelkitching.com.v1.shell-extension.zip
-unzip /tmp/appindicatorsupportrgcjonas.gmail.com.v53.shell-extension.zip -d /usr/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
-unzip /tmp/custom-accent-colorsdemiskp.v6.shell-extension.zip -d /usr/share/gnome-shell/extensions/custom-accent-colors@pantheon.io
-unzip /tmp/soft-brightness-plusjoelkitching.com.v1.shell-extension.zip -d /usr/share/gnome-shell/extensions/soft-brightness-plus@joelkitching.com
-#sudo gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
-#sudo gnome-extensions enable custom-accent-colors@pantheon.io
-#sudo gnome-extensions enable soft-brightness-plus@joelkitching.com
+#unzip /tmp/appindicatorsupportrgcjonas.gmail.com.v53.shell-extension.zip -d /usr/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
+#unzip /tmp/custom-accent-colorsdemiskp.v6.shell-extension.zip -d /usr/share/gnome-shell/extensions/custom-accent-colors@pantheon.io
+#unzip /tmp/soft-brightness-plusjoelkitching.com.v1.shell-extension.zip -d /usr/share/gnome-shell/extensions/soft-brightness-plus@joelkitching.com
+sudo gnome-extensions install /tmp/appindicatorsupportrgcjonas.gmail.com.v53.shell-extension.zip 
+# appindicatorsupport@rgcjonas.gmail.com
+sudo gnome-extensions install /tmp/custom-accent-colorsdemiskp.v6.shell-extension.zip
+#custom-accent-colors@pantheon.io
+sudo gnome-extensions install /tmp/soft-brightness-plusjoelkitching.com.v1.shell-extension.zip
+#soft-brightness-plus@joelkitching.com
