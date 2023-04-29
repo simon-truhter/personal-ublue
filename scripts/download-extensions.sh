5cc29934d6dbc21418e5e43fe25e4d0a7b604968
simon-truhter <simon-truhter@users.noreply.github.com>
2023-04-29 14:14:01 +0200

    tried once more to bake extensions into image

--- scripts/download-extensions.sh
+++ scripts/download-extensions.sh
@@ -1,6 +1,9 @@
 wget -P /usr/share/gnome-shell/extensions https://extensions.gnome.org/extension-data/appindicatorsupportrgcjonas.gmail.com.v53.shell-extension.zip 
 wget -P /usr/share/gnome-shell/extensions https://extensions.gnome.org/extension-data/custom-accent-colorsdemiskp.v6.shell-extension.zip
 wget -P /usr/share/gnome-shell/extensions https://extensions.gnome.org/extension-data/soft-brightness-plusjoelkitching.com.v1.shell-extension.zip
+gnome-extensions install /usr/share/gnome-shell/extensions/appindicatorsupportrgcjonas.gmail.com.v53.shell-extension.zip
+gnome-extensions install /usr/share/gnome-shell/extensions/custom-accent-colorsdemiskp.v6.shell-extension.zip
+gnome-extensions install /usr/share/gnome-shell/extensions/soft-brightness-plusjoelkitching.com.v1.shell-extension.zip
 # nope extensions need to be added to the user space
 #unzip /tmp/appindicatorsupportrgcjonas.gmail.com.v53.shell-extension.zip -d /usr/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
 #unzip /tmp/custom-accent-colorsdemiskp.v6.shell-extension.zip -d /usr/share/gnome-shell/extensions/custom-accent-colors@pantheon.io
