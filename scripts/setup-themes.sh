53b56af2a8160308fbab7e1ddc46ad3abf82675d
simon-truhter <simon-truhter@users.noreply.github.com>
2023-04-29 05:12:32 +0200

    added user setup themes into yafti

--- scripts/setup-themes.sh
+++ scripts/setup-themes.sh
@@ -1,4 +1,5 @@
-sudo git clone https://gitlab.com/garuda-linux/themes-and-settings/artwork/beautyline /usr/share/icons/
-sudo gsettings set org.gnome.desktop.interface icon-theme 'Beautyline'
-sudo gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-sudo gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+sudo git clone https://gitlab.com/garuda-linux/themes-and-settings/artwork/beautyline /usr/share/icons/beautyline
+# stuff needs to be run in yafti
+#sudo gsettings set org.gnome.desktop.interface icon-theme 'Beautyline'
+#sudo gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+#sudo gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
